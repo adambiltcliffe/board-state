6c687729d96f1ce0a205b46d86d6a70aadf500ea
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2019-06-06 20:13:30 +0100

    added (failing) test to highlight issue in array handling

--- game.test.js
+++ game.test.js
@@ -1,8 +1,23 @@
-import Game from './game'
+import Game from "./game";
 
 // Most of the core functionality is tested by testing the examples instead
 
-test('If getFilters() is not overridden, filter() returns the input unchanged', () => {
-  const before = { hi: 'bye' }
-  expect(Game.filter(before)).toEqual(before)
-})
+test("If getFilters() is not overridden, filter() returns the input unchanged", () => {
+  const before = { hi: "bye" };
+  expect(Game.filter(before)).toEqual(before);
+});
+
+test("Complicated test to track down immer issue", () => {
+  class CardPlayGame extends Game {
+    static updateState(state, action) {
+      this.applyUpdate(state, fs => {
+        const pos = fs.hands[action.player].indexOf(action.value);
+        fs.hands[action.player].splice(pos, 1);
+      });
+    }
+  }
+  const s1 = { hands: { c: [9, 10] } };
+  const { state: s2 } = CardPlayGame.playAction(s1, { player: "c", value: 9 });
+  expect(s2).toEqual({ hands: { c: [10] } });
+  const { state: s3 } = CardPlayGame.playAction(s2, { player: "c", value: 10 });
+});
